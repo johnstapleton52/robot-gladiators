d9e426ae89be72111408a9dfbd32e329c959a21b
John Stapleton <johnstapleton52@gmail.com>
2021-08-25 11:26:20 -0700

    added enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,19 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// Game States
+// "WIN" - player robto has defeated all enemy-robots
+//  * Fight all enemy robots
+//  * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 console.log(playerName, playerAttack, playerHealth);
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     alert("GET READY FOR ROUND 1")
@@ -57,5 +63,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 };
 
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
 
-fight();
+
+// fight();
